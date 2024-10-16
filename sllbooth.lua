d5225622620a2d5ced5f5a0f8276f14db45b3c7b
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2024-10-16 18:55:43 +0200

    Update sllbooth.lua

--- sllbooth.lua
+++ sllbooth.lua
@@ -2,62 +2,54 @@ script_key = "uLyVULZDSfDGQdRhudEXYyWXlxvOerXo"
 getgenv().Settings = {
     Sniper = {
         Active = false,
-
         Items = {
-            ["All Rarity: Exotic"] = {Class = "Pet", Price = "80%"},
-            ["All Rarity: Divine"] = {Class = "Pet", Price = 1000},
-            ["Huge Dog"] = {Class = "Pet", "20%"},
+            ["All Rarity: Superior"] = {Class = "Pet", Price = "0%"},
+            ["All Rarity: Divine"] = {Class = "Pet", Price = "0%"},
+            ["All Rarity: Exotic"] = {Class = "Pet", Price = "0%"},
+            ["All Rarity: Mythical"] = {Class = "Pet", Price = "0"},
         },
         Serverhop = {
             ["Switch Servers"] = true,
             ["Teleport Delay (s)"] = 3,
-            ["Save # Servers"] = 10,
+            ["Save # Last Joined Servers"] = 10,
         },
-    
         Webhook = {
             ["URL"] = "https://discord.com/api/webhooks/1246125041473814680/9fseVCyXc34XSpXki4TBt5FwamZGKqiiDApYILv84U5LC-Nuoav9EgfwpwOR1inDh7Cf",
             ["Send Embeds"] = true,
             ["Remove Username"] = true,
-            ["Ping on Huges 'n Titanics"] = true,
             ["Global Snipes"] = true,
         },
-
         StopParams = {
             ["Limits Reached"] = false,
             ["Diamonds Hit: 250k"] = false,
             ["60 Minutes"] = false,
-            ["Switch To Selling"] = true,
+            ["Switch To Selling"] = false,
         },
     },
-
     Seller = {
         Active = true,
-
         Items = {
-            ["All Rarity: Superior"] = {Class = "Pet", Price = "-3%"},
-            ["All Rarity: Divine"] = {Class = "Pet", Price = "-3%"},
-            ["All Rarity: Exotic"] = {Class = "Pet", Price = "-3%"},
-            ["All Rarity: Mythical"] = {Class = "Pet", Price = "-3%"},
+            --// Example Settings, everything is editable.
+            ["All Rarity: Superior"] = {Class = "Pet", Price = "5%"},
+            ["All Rarity: Divine"] = {Class = "Pet", Price = "5%"},
+            ["All Rarity: Exotic"] = {Class = "Pet", Price = "5%"},
+            ["All Rarity: Mythical"] = {Class = "Pet", Price = "5%"},
         },
-
         Serverhop = {
             ["Switch Servers"] = true,
             ["Teleport Delay (m)"] = 15,
         },
-
         Webhook = {
             ["URL"] = "https://discord.com/api/webhooks/1246125041473814680/9fseVCyXc34XSpXki4TBt5FwamZGKqiiDApYILv84U5LC-Nuoav9EgfwpwOR1inDh7Cf",
             ["Send Embeds"] = true,
             ["Remove Username"] = true,
         },
-
         StopParams = {
             ["Item Runout"] = true,
             ["Diamonds Hit: 1b"] = false,
             ["60 Minutes"] = false,
             ["Switch To Sniping"] = false,
         },
-
         Other = {
             ["Auto Accept Mail"] = true,
             ["Diamonds Hit: 1b Sendout"] = "NomStock",
@@ -65,7 +57,5 @@ getgenv().Settings = {
             ["Never Join Friendslist"] = false,
         },
     },
-    
-    [[ Thank you for using System Exodus <3! ]]
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/717430e3bbde3530feb824de729fcc90.lua"))()
